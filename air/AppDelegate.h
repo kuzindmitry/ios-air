9f06375a3465630627182049ee2406c518d7fa40
Dmitry Kuzin <dmitry@kuzin.es>
2017-10-17 13:00:39 +0300

    Initial Commit

---
+++ air/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  air
+//
+//  Created by kuzindmitry on 17.10.2017.
+//  Copyright © 2017 kuzindmitry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
